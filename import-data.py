a191ecc463609a37bcc4c65899a8d299ed4cddd7
rae-jet-aime <azuronyx@gmail.com>
2025-03-26 14:07:51 -0700

    Importing data necessary for midterm labs.

---
+++ import-data.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 26 14:05:23 2025
+
+@author: raesheng
+"""
+import numpy as np
+data_set = np.loadtxt("HIVseries.csv", delimiter = ",")
